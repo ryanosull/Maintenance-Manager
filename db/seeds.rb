82824ccefa32c7cf391784b2dae08e900cda7004
William Ryan OSullivan <wrosullivan@gmail.com>
2023-06-06 14:36:47 -0500

    cleanup on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,9 @@ o2 = Owner.create(name: "Joe Decker", total_units_owned: 2)
 o3 = Owner.create(name: "Keith Strausser", total_units_owned: 1)
 o4 = Owner.create(name: "Clay Henry", total_units_owned: 1)
 o5 = Owner.create(name: "Bob Brady", total_units_owned: 1)
-puts "SLUMLORDS UNITE!"
+puts "OWNERS seeded"
 
-puts "seeding maintenance requests"
+puts "seeding maintenance requests..."
 m1 = MaintenanceRequest.create(urgency: "low", description: "back porch light not working", expected_cost: 150.00, actual_cost: 5.29, date_opened: "2022-09-17", date_closed: "2022-09-17" )
 m2 = MaintenanceRequest.create(urgency: "medium", description: "freezer not staying cold", expected_cost: 250.00, actual_cost: 752.36, date_opened: '2022-10-16', date_closed: '2022-10-16' )
 m3 = MaintenanceRequest.create(urgency: "high", description: "front window glass broken", expected_cost: 100.00, actual_cost: 125.98, date_opened: '2022-01-09', date_closed: '2022-01-09' )
@@ -23,10 +23,10 @@ m5 = MaintenanceRequest.create(urgency: "medium", description: "gfci outlet in b
 m6 = MaintenanceRequest.create(urgency: "high", description: "smoke coming from furnace", expected_cost: 500.00, actual_cost: 1385.69, date_opened: '2022-11-08', date_closed: '2022-11-11' )
 m7 = MaintenanceRequest.create(urgency: "low", description: "critter scurrying sound coming from attic", expected_cost: 40.00, actual_cost: 20.00, date_opened: '2022-09-17', date_closed: '2022-09-17' )
 m8 = MaintenanceRequest.create(urgency: "medium", description: "front storm door off hinges", expected_cost: 60.00, actual_cost: 20.25, date_opened: '2022-09-17', date_closed: '2022-09-17' )
-puts "TENANTS DRIVE ME NUTS"
+puts "MAINTENANCE REQS SEEDED"
 
 
-puts "seeding units"
+puts "seeding units..."
 u1 = Unit.create(address: "123 Fake Street Columbia, MO 65203", current_tenant: "Jim Jones", open_request?: true, owner_id: o3.id, maintenancerequest_id: m1.id)
 u2 = Unit.create(address: "4585 North Ave. Jefferson City, MO 65111", current_tenant: "Katy Katters", open_request?: false, owner_id: o2.id, maintenancerequest_id: m1.id)
 u3 = Unit.create(address: "13898 SE Rollins Rd. Columbia, MO 65203", current_tenant: "Mike Meddler", open_request?: true, owner_id: o4.id, maintenancerequest_id: m3.id)
@@ -35,7 +35,7 @@ u5 = Unit.create(address: "666 Unlucky Ave. Jefferson City, MO 65111", current_t
 u6 = Unit.create(address: "1 Broadway Blvd. Unit 1 Columbia, MO 65201", current_tenant: "Timmy Taylor", open_request?: false, owner_id: o1.id, maintenancerequest_id: m6.id)
 u7 = Unit.create(address: "1 Broadway Blvd. Unit 2 Columbia, MO 65201", current_tenant: "Tammy Taylor", open_request?: true, owner_id: o1.id, maintenancerequest_id: m7.id)
 u8 = Unit.create(address: "420 Pleasant St. Columbia, MO 65203", current_tenant: "Bob Marley", open_request?: false, owner_id: o1.id, maintenancerequest_id: m8.id)
-puts 'NO VACANT UNITS'
+puts 'UNITS seeded'
 
 
 puts "✅ Done seeding!"
