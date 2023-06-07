4d990c362df7b999efcedf901c1cf0ae6e8dcba3
William Ryan OSullivan <wrosullivan@gmail.com>
2023-06-06 20:18:48 -0500

    seed data refactored, faker gem used

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 puts "🌱 Seeding..."
 
 puts "destroying all...."
@@ -8,33 +10,53 @@ puts "DESTROYED!"
 
 
 puts "seeding maintenance requests..."
-m1 = MaintenanceRequest.create(urgency: "low", description: "back porch light not working", expected_cost: 150.00, actual_cost: 5.29, date_opened: "2022-09-17", date_closed: nil, unit_id: u1.id )
-m2 = MaintenanceRequest.create(urgency: "medium", description: "freezer not staying cold", expected_cost: 250.00, actual_cost: 752.36, date_opened: '2022-10-16', date_closed: nil, unit_id: u2.id )
-m3 = MaintenanceRequest.create(urgency: "high", description: "front window glass broken", expected_cost: 100.00, actual_cost: 125.98, date_opened: '2022-01-09', date_closed: nil, unit_id: u3.id )
-m4 = MaintenanceRequest.create(urgency: "low", description: "lots of ants on back deck", expected_cost: 15.00, actual_cost: 15.00, date_opened: '2022-06-03', date_closed: nil, unit_id: u3.id )
+m1 = MaintenanceRequest.create(urgency: "low", description: "back porch light not working", expected_cost: 150.00, actual_cost: 5.29, date_opened: "2023-05-17", date_closed: nil, unit_id: u1.id )
+
+m2 = MaintenanceRequest.create(urgency: "medium", description: "freezer not staying cold", expected_cost: 250.00, actual_cost: 752.36, date_opened: '2023-02-16', date_closed: nil, unit_id: u2.id )
+
+m3 = MaintenanceRequest.create(urgency: "high", description: "front window glass broken", expected_cost: 100.00, actual_cost: 125.98, date_opened: '2022-12-09', date_closed: nil, unit_id: u3.id )
+
+m4 = MaintenanceRequest.create(urgency: "low", description: "lots of ants on back deck", expected_cost: 15.00, actual_cost: 15.00, date_opened: '2022-11-03', date_closed: nil, unit_id: u3.id )
+
 m5 = MaintenanceRequest.create(urgency: "medium", description: "gfci outlet in bathroom keeps resetting", expected_cost: 20.00, actual_cost: 20.00, date_opened: '2022-12-26', date_closed: nil, unit_id: u2.id )
-m6 = MaintenanceRequest.create(urgency: "high", description: "smoke coming from furnace", expected_cost: 500.00, actual_cost: 1385.69, date_opened: '2022-11-08', date_closed: nil, unit_id: u4.id)
-m7 = MaintenanceRequest.create(urgency: "low", description: "critter scurrying sound coming from attic", expected_cost: 40.00, actual_cost: 20.00, date_opened: '2022-09-17', date_closed: nil, unit_id: u5.id)
-m8 = MaintenanceRequest.create(urgency: "medium", description: "front storm door off hinges", expected_cost: 60.00, actual_cost: 20.25, date_opened: '2022-09-17', date_closed: nil, unit_id: u5.id)
+
+m6 = MaintenanceRequest.create(urgency: "high", description: "smoke coming from furnace", expected_cost: 500.00, actual_cost: 1385.69, date_opened: '2023-01-08', date_closed: nil, unit_id: u4.id)
+
+m7 = MaintenanceRequest.create(urgency: "low", description: "critter scurrying sound coming from attic", expected_cost: 40.00, actual_cost: 20.00, date_opened: '2023-02-17', date_closed: '2023-02-19', unit_id: u5.id)
+
+m8 = MaintenanceRequest.create(urgency: "medium", description: "front storm door off hinges", expected_cost: 60.00, actual_cost: 20.25, date_opened: '2023-03-17', date_closed: '2023-03-18', unit_id: u5.id)
 puts "MAINTENANCE REQS SEEDED"
 
+
+
 puts "seeding owners..."
-o1 = Owner.create(name: "Kyle Rieman", phone_number: "(111)-111-") #lets get faker gem involved here and in other places. perhaps money gem as well.
-o2 = Owner.create(name: "Joe Decker", phone_number: 2)
-o3 = Owner.create(name: "Keith Strausser", phone_number: 1)
-o4 = Owner.create(name: "Clay Henry", phone_number: 1)
-o5 = Owner.create(name: "Bob Brady", phone_number: 1)
+o1 = Owner.create(name: "Kyle Rieman", phone_number: Faker::PhoneNumber.cell_phone)
+
+o2 = Owner.create(name: "Joe Decker", phone_number: Faker::PhoneNumber.cell_phone)
+
+o3 = Owner.create(name: "Keith Strausser", phone_number: Faker::PhoneNumber.cell_phone)
+
+o4 = Owner.create(name: "Clay Henry", phone_number: Faker::PhoneNumber.cell_phone)
+
+o5 = Owner.create(name: "Bob Brady", phone_number: Faker::PhoneNumber.cell_phone)
 puts "OWNERS seeded"
 
 puts "seeding units..."
-u1 = Unit.create(address: "123 Fake Street Columbia, MO 65203", current_tenant: "Jim Jones", open_request?: true, owner_id: o3.id, maintenancerequest_id: m1.id)
-u2 = Unit.create(address: "4585 North Ave. Jefferson City, MO 65111", current_tenant: "Katy Katters", open_request?: false, owner_id: o2.id, maintenancerequest_id: m1.id)
-u3 = Unit.create(address: "13898 SE Rollins Rd. Columbia, MO 65203", current_tenant: "Mike Meddler", open_request?: true, owner_id: o4.id, maintenancerequest_id: m3.id)
-u4 = Unit.create(address: "411 River Rd. Rocheport, MO 65279", current_tenant: "Brian Bukowski", open_request?: false, owner_id: o5.id, maintenancerequest_id: m4.id)
-u5 = Unit.create(address: "666 Unlucky Ave. Jefferson City, MO 65111", current_tenant: "Jack Black", open_request?: false, owner_id: o2.id, maintenancerequest_id: m5.id)
-u6 = Unit.create(address: "1 Broadway Blvd. Unit 1 Columbia, MO 65201", current_tenant: "Timmy Taylor", open_request?: false, owner_id: o1.id, maintenancerequest_id: m6.id)
-u7 = Unit.create(address: "1 Broadway Blvd. Unit 2 Columbia, MO 65201", current_tenant: "Tammy Taylor", open_request?: true, owner_id: o1.id, maintenancerequest_id: m7.id)
-u8 = Unit.create(address: "420 Pleasant St. Columbia, MO 65203", current_tenant: "Bob Marley", open_request?: false, owner_id: o1.id, maintenancerequest_id: m8.id)
+u1 = Unit.create(address: "123 Fake Street Columbia, MO 65203", current_tenant: "Jim Jones", open_request?: true, owner_id: o3.id)
+
+u2 = Unit.create(address: "4585 North Ave. Jefferson City, MO 65111", current_tenant: "Katy Katters", open_request?: false, owner_id: o2.id)
+
+u3 = Unit.create(address: "13898 SE Rollins Rd. Columbia, MO 65203", current_tenant: "Mike Meddler", open_request?: true, owner_id: o4.id)
+
+u4 = Unit.create(address: "411 River Rd. Rocheport, MO 65279", current_tenant: "Brian Bukowski", open_request?: false, owner_id: o5.id)
+
+u5 = Unit.create(address: "666 Unlucky Ave. Jefferson City, MO 65111", current_tenant: "Jack Black", open_request?: false, owner_id: o2.id)
+
+u6 = Unit.create(address: "1 Broadway Blvd. Unit 1 Columbia, MO 65201", current_tenant: "Timmy Taylor", open_request?: false, owner_id: o1.id)
+
+u7 = Unit.create(address: "1 Broadway Blvd. Unit 2 Columbia, MO 65201", current_tenant: "Tammy Taylor", open_request?: true, owner_id: o1.id)
+
+u8 = Unit.create(address: "420 Pleasant St. Columbia, MO 65203", current_tenant: "Bob Marley", open_request?: false, owner_id: o1.id)
 puts 'UNITS seeded'
 
 puts "✅ Done seeding!"
